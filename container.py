b2433d7a21a57f4b5b5eb162688cd9f573308344
Jonathan Bowman <bowmanjd@lancastermennonite.org>
2018-12-26 08:25:10 -0500

    better tagging

--- container.py
+++ container.py
@@ -2,7 +2,6 @@
 
 import subprocess
 import sys
-import time
 
 import fire
 
@@ -16,28 +15,40 @@ class Container:
             self.docker_exe.insert(0, "sudo")
         self.location = f"{repo}/{name}"
 
-    def build(self, filename="Dockerfile", base="ubuntu:bionic", clean=False):
+    def build(self, filename="Dockerfile", base=None, clean=False, push=True):
+        if not base:
+            base = "debian:stable-slim"
+            default_base = True
+        else:
+            default_base = False
         with open(filename) as f:
             tpl = f.read()
         base_cmd = self.docker_exe + ["build"]
         if clean:
             base_cmd.append("--no-cache")
-        version = time.strftime("%y%m%d.%H%M")
         for arch, prefix in arches.items():
             cmd = base_cmd.copy()
-            tags = [arch, arch + "-" + base.replace(":", "-")]
+            tags = []
+            if default_base:
+                tags.append(arch)
+                if arch == "i386":
+                    tags.append("latest")
+            tags.append(arch + "-" + base.replace(":", "-"))
             for tag in tags:
                 cmd.extend(["-t", f"{self.location}:{tag}"])
             cmd.append("-")
             dockerfile = tpl.format(base=prefix + base, arch=arch)
             subprocess.run(cmd, input=dockerfile, encoding="utf-8")
+            if push:
+                for tag in tags:
+                    self.push(f"{self.location}:{tag}")
 
     def push(self, location):
         cmd = self.docker_exe + ["push", location]
         subprocess.run(cmd)
 
     def run(self):
-        cmd = self.docker_exe + ["run", "-it", "--rm", self.location]
+        cmd = self.docker_exe + ["run", "-it", self.location]
         subprocess.run(cmd)
 
 
